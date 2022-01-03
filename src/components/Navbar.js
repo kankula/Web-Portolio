91c6bddc51be581e1220a694b689a50e2295de1a
kankula <kiri0944@gmail.com>
2022-01-04 00:52:02 +0700

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,23 @@ const Navbar = () => {
         const header = document.querySelector('nav');
         header.classList.toggle("sticky", window.scrollY > 0);
     });
+
+    const menuToggle = () => {
+        document.getElementById('nav-menu').classList.toggle('active')
+    }
+    const removeActive = () => {
+        document.getElementById('nav-menu').classList.remove('active')
+    }
     return (
         
         <nav id="navbar" className='navbar'>
             <div className='max-width'>
                 <div className='logo'>
-                    <a href='#home'><h2>Portfoli<span>o</span></h2></a>
+                    <a href='#home' onClick={removeActive}><h2>Portfoli<span>o</span></h2></a>
                 </div>
-                <ul className='nav-menu'>
-                {navItem.map(navItem => <li key={navItem.id}><a href={navItem.id}>{navItem.title}</a></li>)}
+                <i className="fas fa-align-justify" onClick={menuToggle}></i>
+                <ul id='nav-menu' className='nav-menu'>
+                {navItem.map(navItem => <li key={navItem.id}><a href={navItem.id} onClick={removeActive}>{navItem.title}</a></li>)}
                 </ul>
             </div>
         </nav>
